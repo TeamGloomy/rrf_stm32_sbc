a75452781373e8aec800aa07e7590083d2a26574
Loïc GRENON <difool@difoolou.net>
2023-08-30 18:32:13 +0200

    Add build script

---
+++ build.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+if [ $# -lt 1 ]; then
+    echo "Usage: $0 <board>"
+    exit 1
+fi
+
+BOARD=$1
+RRF_VERSION="RRF-3.4.6"
+MAINTAINER="TeamGloomy"
+MAINTAINERMAIL="teamgloomyrrf@gmail.com"
+RELEASE_NAME=jammy
+KERNEL_BRANCH="current"
+
+# Checkout Armbian build script
+git clone --depth 1 https://github.com/armbian/build 
+
+# Merge supported boards into Armbian build environment
+mkdir -p ./build/userpatches
+cp -R ./armbian/userpatches/* ./build/userpatches/
+cp -R ./armbian/config/boards/* ./build/config/boards/
+
+# cleaning leftovers if any
+rm -rf ./build/output/images/*
+
+cd build
+./compile.sh VENDOR="${RRF_VERSION}" MAINTAINER="${MAINTAINER}" MAINTAINERMAIL="${MAINTAINERMAIL}" BOARD="${BOARD}" BRANCH="${KERNEL_BRANCH}" RELEASE="${RELEASE_NAME}" BUILD_MINIMAL=no KERNEL_CONFIGURE=no KERNEL_GIT=full BOOTFS_TYPE=fat COMPRESS_OUTPUTIMAGE=sha,xz
+
+# Possible options :
+# BUILD_DESKTOP=yes DESKTOP_ENVIRONMENT=xfce DESKTOP_ENVIRONMENT_CONFIG_NAME=config_base
+# BOOTBRANCH='tag:v2021.01
+# KERNELBRANCH='branch:linux-5.10.y'
